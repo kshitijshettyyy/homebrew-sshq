97b9bea47cccb03aec3e12973ecf359cc7f4d745
Kshitij Kishore Shetty <118621124+kshitijshettyyy@users.noreply.github.com>
2025-11-25 14:53:19 +0530

    Update SSHQ to version 1.0.3

--- sshq.rb
+++ sshq.rb
@@ -1,8 +1,8 @@
 class Sshq < Formula
   desc "SSH Simplified for Devs"
   homepage "https://github.com/kshitijshettyyy/sshq"
-  url "https://github.com/kshitijshettyyy/sshq/releases/download/v1.0.2/sshq"
-  sha256 "f2587b07486d6ac73d55695c66c82b480638afe9a4e560af3d66c74f136cfb61"
+  url "https://github.com/kshitijshettyyy/sshq/releases/download/v1.0.3/sshq"
+  sha256 "4b5b37c620b62076f072b1d5907676176f28ef8a6e485a6ac67411e91024e81c"
   license "MIT"
 
   def install
